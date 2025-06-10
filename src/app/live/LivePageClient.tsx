21a47b1b2d304e7b5e1137628a979c74909373b1
Danilo <danilo.dev6@gmail.com>
2025-06-10 11:05:33 -0300

    add import live page

--- src/app/live/LivePageClient.tsx
+++ src/app/live/LivePageClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { Countdown } from "@/components/Countdown";
 import { Button } from "@/components/ui/button";
