8de7763858bd22e040e3a075e649c73e6b2fdb2f
Danilo <danilo.dev6@gmail.com>
2025-06-19 19:28:21 -0300

    add terms and datadel pages

---
+++ src/app/data-deletion/page.tsx
@@ -0,0 +1,8 @@
+export default function DataDeletion() {
+  return (
+    <div className="p-8">
+      <h1>Data Deletion Instructions</h1>
+      <p>Instructions for data deletion - Development Version</p>
+    </div>
+  );
+}
---
+++ src/app/terms/page.tsx
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <div className="p-8">
+      <h1>Terms of Service</h1>
+      <p>Terms of service for Auction App - Development Version</p>
+    </div>
+  );
+}
