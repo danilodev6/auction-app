86d2b48fc3ee7a71409a50e515c92a03d3f65cb0
Danilo <danilo.dev6@gmail.com>
2025-06-19 21:57:12 -0300

    env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,32 +3,50 @@
 //
 // export const env = createEnv({
 //   server: {
+//     NODE_ENV: z.enum(["development", "test", "production"]),
 //     DATABASE_URL: z.string().url(),
-//     NODE_ENV: z.string().min(1),
 //   },
-//   client: {},
+//   client: {
+//     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+//     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+//   },
 //   runtimeEnv: {
 //     NODE_ENV: process.env.NODE_ENV,
 //     DATABASE_URL: process.env.DATABASE_URL,
+//     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+//     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 //   },
 // });
-
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string().min(1),
+    AUTH_GOOGLE_ID: z.string().min(1),
+    AUTH_GOOGLE_SECRET: z.string().min(1),
+    AUTH_FACEBOOK_ID: z.string().min(1),
+    AUTH_FACEBOOK_SECRET: z.string().min(1),
+    NEXTAUTH_URL: z.string().url().optional(),
   },
   client: {
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
+    NEXT_PUBLIC_PUSHER_KEY: z.string().min(1).optional(),
+    NEXT_PUBLIC_PUSHER_CLUSTER: z.string().min(1).optional(),
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    AUTH_FACEBOOK_ID: process.env.AUTH_FACEBOOK_ID,
+    AUTH_FACEBOOK_SECRET: process.env.AUTH_FACEBOOK_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
   },
 });
