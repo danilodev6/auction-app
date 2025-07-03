cba01acf54f7e5354eb58997131bc389660d6838
Danilo <danilo.dev6@gmail.com>
2025-07-03 20:33:02 -0300

    fix Pagination in manage items

--- src/app/items/manage/Pagination.tsx
+++ src/app/items/manage/Pagination.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
@@ -10,23 +10,35 @@ interface PaginationProps {
 }
 
 const Pagination: React.FC<PaginationProps> = ({ currentPage, pageSize }) => {
-  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  // Convert existing search params to a plain object
+  const params = new URLSearchParams(searchParams.toString());
 
   return (
     <div className="flex justify-center mt-4">
       <nav className="flex items-center">
         <Link
           href={{
-            pathname: router.pathname,
-            query: { ...router.query, page: currentPage - 1, pageSize },
+            pathname,
+            query: {
+              ...Object.fromEntries(params),
+              page: currentPage - 1,
+              pageSize,
+            },
           }}
         >
           <Button disabled={currentPage <= 1}>Previous</Button>
         </Link>
         <Link
           href={{
-            pathname: router.pathname,
-            query: { ...router.query, page: currentPage + 1, pageSize },
+            pathname,
+            query: {
+              ...Object.fromEntries(params),
+              page: currentPage + 1,
+              pageSize,
+            },
           }}
         >
           <Button className="mx-3">Next</Button>
