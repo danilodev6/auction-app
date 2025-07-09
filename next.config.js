11dc7e7854008effdba1cddd47a98459dbd06d72
Danilo <danilo.dev6@gmail.com>
2025-07-09 19:15:32 -0300

    .

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dxvshlewdqyqbqqktqph.supabase.co",
+        hostname: "oegkjipmpbucywefggtm.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
